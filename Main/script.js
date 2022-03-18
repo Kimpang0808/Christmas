c27f0684cafa6ff3efaef527d7741f3f16f01e16
S0DA04 <92915929+S0DA04@users.noreply.github.com>
2022-03-18 22:48:43 +0900

    scroll controll
    but it doesn't works

---
+++ Main/script.js
@@ -0,0 +1,26 @@
+window.addEventListener("wheel", function(e){
+	e.preventDefault();
+},{passive : false});
+var $html = $("index.html");
+var page = 1;
+var lastPage = $(".Slideindex").length;
+
+$html.animate({scrollTop:0},10);
+$(window).on("wheel", function(e){
+ 
+	if($html.is(":animated")) return;
+ 
+	if(e.originalEvent.deltaY > 0){
+		if(page== lastPage) return;
+ 
+		page++;
+	}else if(e.originalEvent.deltaY < 0){
+		if(page == 1) return;
+ 
+		page--;
+	}
+	var posTop = (page-1) * $(window).height();
+ 
+	$html.animate({scrollTop : posTop});
+ 
+});
