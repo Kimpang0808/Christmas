f66980aed4bc6ec2665e396b03d9de52cdc9677e
gigigi8264 <seohyun8264@naver.com>
2022-03-18 22:59:28 +0900

    이동오류 및 주석수정

--- Main/script.js
+++ Main/script.js
@@ -24,4 +24,4 @@ $(window).on("wheel", function(e){
 	$html.animate({scrollTop : posTop});
  
 });
-\\https://velog.io/@sklove96/%ED%95%9C-%ED%8E%98%EC%9D%B4%EC%A7%80%EC%94%A9-%EC%98%AC%EB%9D%BC%EA%B0%80%EB%8A%94-%EC%8A%A4%ED%81%AC%EB%A1%A4
+//https://velog.io/@sklove96/%ED%95%9C-%ED%8E%98%EC%9D%B4%EC%A7%80%EC%94%A9-%EC%98%AC%EB%9D%BC%EA%B0%80%EB%8A%94-%EC%8A%A4%ED%81%AC%EB%A1%A4
